4fdef7bc3ba55c99cd66b46fda7cc772502099c3
yosefkahlon <yosefkahlon53@gmail.com>
2022-05-22 12:53:59 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/orgchart_b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orgchart_b.dir/main.cpp.o"
+  "orgchart_b"
+  "orgchart_b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orgchart_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/orgchart_b.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for orgchart_b.
+# This may be replaced when dependencies are built.
